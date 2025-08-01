0b8b87426b6fb10699315d35600b61aa3d8c16da
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2025-08-01 14:25:54 +0200

    chapter 6 started

--- 05_order_by_group_by.sql
+++ 05_order_by_group_by.sql
@@ -1,21 +1,55 @@
-SELECT year,
-       month,
-       count(*) AS record_count
-  FROM station_data
- WHERE tornado = 1
-group by year, month;
-  
-SELECT year,
-       month,
-       count(*) AS record_count
-  FROM station_data
- WHERE tornado = 1
-group by 1, 2;
+-- count()
+SELECT count(*) AS record_count
+FROM station_data;
 
-SELECT year,
-       month,
-       count(*) AS record_count
-  FROM station_data
- WHERE tornado = 1
-group by year, month
-order by year DESC, month DESC;
+
+-- count tornado records
+SELECT count(*) AS record_count
+FROM station_data
+WHERE tornado = 1;
+
+
+-- count tornado records for every year
+SELECT
+    year
+  , count(*) AS record_count
+FROM station_data
+WHERE tornado = 1
+GROUP BY year;
+
+
+-- for every month of a year
+SELECT
+    year
+  , month
+  , count(*) AS record_count
+FROM station_data
+WHERE tornado = 1
+GROUP BY
+    year
+  , month;
+
+
+-- same using ordinal position instead of column names
+SELECT
+    year
+  , month
+  , count(*) AS record_count
+FROM station_data
+WHERE tornado = 1
+GROUP BY 1, 2;
+
+
+-- descendent order
+SELECT
+    year
+  , month
+  , count(*) AS record_count
+FROM station_data
+WHERE tornado = 1
+GROUP BY
+    year
+  , month
+ORDER BY
+    year DESC
+  , month DESC;
