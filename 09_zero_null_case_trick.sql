cbd053f656c8ed2ce10c09d4c59384426d85ecdf
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2025-08-05 12:04:59 +0200

    chapter 7 finished

--- 09_zero_null_case_trick.sql
+++ 09_zero_null_case_trick.sql
@@ -5,8 +5,15 @@ SELECT year, month,
  WHERE tornado = 1
    AND year >= 1990
 GROUP BY year, month;
+-- precipitation if a tornado was NOT present
+SELECT year, month,
+       SUM(precipitation) AS non_tornado_precipitation
+  FROM station_data
+ WHERE tornado = 0
+   AND year >= 1990
+GROUP BY year, month;
 
--- both, precipitation when a tornado was present and precipitation when no tornado was present
+-- both in one statement
 SELECT year, month,
 SUM(CASE WHEN tornado = 1 THEN precipitation ELSE 0 END) AS tornado_precipitation,
 SUM(CASE WHEN tornado = 0 THEN precipitation ELSE 0 END) AS non_tornado_precipitation
@@ -14,10 +21,25 @@ FROM station_data
 WHERE year >= 1990
 GROUP BY year, month;
 
--- use NULL instead of 0 for aggregate functions that could be influenced by zero
+
+-- when searching for MIN / MAX use NULL instead of 0
+-- NULLs are ignored, while 0 could be the miniumum or maximum value
 SELECT year, month,
 MAX(CASE WHEN tornado = 1 THEN precipitation ELSE NULL END) AS max_tornado_precipitation,
 MAX(CASE WHEN tornado = 0 THEN precipitation ELSE NULL END) AS max_non_tornado_precipitation
 FROM station_data
 WHERE year >= 1990
-GROUP BY year, month;
+GROUP BY year, month
+
+
+
+-- AND, OR and NOT can also be use inside a CASE statement
+SELECT month,
+    AVG(CASE WHEN rain OR hail THEN temperature ELSE NULL END) AS avg_precipitation_temp,
+    AVG(CASE WHEN NOT (rain OR hail) THEN temperature ELSE NULL END) AS avg_non_precipitation_temp
+FROM STATION_DATA
+WHERE year >= 2000
+GROUP BY month;
+
+
+
